59cff2d8913033a557c9346122b2eca0196c3b05
Jose Pedro Aguiar Ferreira <jose.ferreira@terceiros.solides.com.br>
2026-03-21 21:30:24 -0300

    AM-2024-12 - Fixing bugs

--- src/Components/OrdemServico/FormOrdemServico/AtualizarCabecalhoForm.js
+++ src/Components/OrdemServico/FormOrdemServico/AtualizarCabecalhoForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Formik, ErrorMessage, Field} from 'formik';
+import { Formik, ErrorMessage, Field } from 'formik';
 import FormControlInput from '../../FormControl/index.js'
 import FormControlSelect from '../../FormControl/Select.js'
-import {Col, Row, Tabs, Tab } from 'react-bootstrap';
+import { Col, Row, Tabs, Tab } from 'react-bootstrap';
 import Button from '../../FormControl/Button.js';
 import estilos from './FormOrdemServico.module.css'
 import Modal from '../../Utils/Modal/index.js'
 import useFetch from '../../../Hooks/useFetch.js';
-import {UserContex} from '../../../Context/UserContex.js'
+import { UserContex } from '../../../Context/UserContex.js'
 import Required from '../../FormControl/Required.js';
 import Load from '../../Utils/Load/index.js'
 import AlertaDismissible from '../../Utils/Alerta/AlertaDismissible.js'
@@ -16,189 +16,163 @@ import FormOrdemServicoCobrancas from '../FormOrdemServicoCobrancas/index.js'
 import Swal from 'sweetalert2'
 
 
-import {TOKEN_POST, CLIENT_ID,CLIENT_SECRET, ORDEM_SERVICO_UPDATE_POST, MOTIVOS_CANCEL_OS_ALL_POST} from '../../../api/endpoints/geral.js'
+import { TOKEN_POST, CLIENT_ID, CLIENT_SECRET, ORDEM_SERVICO_UPDATE_POST, MOTIVOS_CANCEL_OS_ALL_POST } from '../../../api/endpoints/geral.js'
 
 
-const AtualizarCabecalhoForm  = ({dataOrdemServicoChoice, setDataOrdemServico, setIdOrdemServico, idOrdemServico, showModalCriarOrdemServico, setShowModalCriarOrdemServico, callback, cancelarOrdemServico, setAtualizarCabecalhoOrdemServico, carregando})=>{
+const AtualizarCabecalhoForm = ({ dataOrdemServicoChoice, setDataOrdemServico, setIdOrdemServico, idOrdemServico, showModalCriarOrdemServico, setShowModalCriarOrdemServico, callback, cancelarOrdemServico, setAtualizarCabecalhoOrdemServico, carregando }) => {
 
-	const {data, error, request, loading} = useFetch();
+	const { data, error, request, loading } = useFetch();
 	const dataRequest = useFetch();
 
-	const {getToken, dataUser} 			= React.useContext(UserContex);
-	const [observacao, setObservacao] 	= React.useState([])
-	const [dataItens, setDataitens]		= React.useState([])
+	const { getToken, dataUser } = React.useContext(UserContex);
+	const [observacao, setObservacao] = React.useState([])
+	const [dataItens, setDataitens] = React.useState([])
 
-	const userLogar =  ()=>{
-        console.log('Aqui............')
-    }
+	const sendData = async ({
+		observacao
+	}) => {
 
-    const sendData = async ({
-			observacao
-		})=>{
-			
-		
 		const data = {
 			observacao
 		}
-		
 
-		const {url, options} = ORDEM_SERVICO_UPDATE_POST(idOrdemServico, data, getToken());
+		const { url, options } = ORDEM_SERVICO_UPDATE_POST(idOrdemServico, data, getToken());
+		const { response, json } = await request(url, options);
 
+		if (json) {
 
-		const {response, json} = await request(url, options);
-		console.log('Save consulta here')
-		console.log(json)
-		if(json){
-			console.log('Response Save consulta here')
-			console.log(json)
-			
 			callback();
 			setShowModalCriarOrdemServico();
 			setAtualizarCabecalhoOrdemServico(false);
 			setIdOrdemServico(null);
 
 			Swal.fire({
-			  icon: "success",
-			  title: "",
-			  text: 'Registrado com sucesso',
-			  footer: '',//'<a href="#">Why do I have this issue?</a>'
-			  confirmButtonColor: "#07B201",
+				icon: "success",
+				title: "",
+				text: 'Registrado com sucesso',
+				footer: '',
+				confirmButtonColor: "#07B201",
 			});
 		}
-    }
+	}
 
-	const requestAllMotivoCancel = async() =>{
-       
-        const {url, options} = MOTIVOS_CANCEL_OS_ALL_POST({}, getToken());
+	const requestAllMotivoCancel = async () => {
 
+		const { url, options } = MOTIVOS_CANCEL_OS_ALL_POST({}, getToken());
+		const { response, json } = await dataRequest.request(url, options);
 
-        const {response, json} = await dataRequest.request(url, options);
-        console.log('All consultas here')
-        console.log(json)
-        if(json){
-            setObservacao(json)
-        }else{
+		if (json) {
+			setObservacao(json)
+		} else {
 
-        	setObservacao([]);
-        }
+			setObservacao([]);
+		}
+	}
 
-            
-    }
+	const preparaMotivoCancelToForm = () => {
+		if (observacao.hasOwnProperty('mensagem') && Array.isArray(observacao.mensagem) && observacao.mensagem.length > 0) {
+			let filiais = observacao.mensagem.map(({ id, motivo }, index, arr) => ({ label: motivo, valor: id, props: {} }))
+			filiais.unshift({ label: 'Selecione...', valor: '', props: { selected: 'selected', disabled: 'disabled' } })
 
-	const preparaMotivoCancelToForm = ()=>{
-    	if(observacao.hasOwnProperty('mensagem') && Array.isArray(observacao.mensagem) && observacao.mensagem.length > 0){
-    		let filiais = observacao.mensagem.map(({id, motivo}, index, arr)=>({label:motivo,valor:id,props:{}}))
-    		filiais.unshift({label:'Selecione...',valor:'',props:{selected:'selected', disabled:'disabled'}})
-    		
-    		return filiais;
-    	}
-    	return []
-    }
+			return filiais;
+		}
+		return []
+	}
 
-	const dataToFormOrdemServico = ()=>{
-    	let obj = {observacao:''}
-    	if(dataOrdemServicoChoice && dataOrdemServicoChoice.hasOwnProperty('mensagem')){
-    		let data = dataOrdemServicoChoice.mensagem;
-			
-    		if(data.hasOwnProperty('observacao')){
-                obj.observacao = data.observacao;
-    		}
-    		
-    	}
+	const dataToFormOrdemServico = () => {
+		let obj = { observacao: '' }
 
-    	return obj;
-    }
+		if (dataOrdemServicoChoice && dataOrdemServicoChoice.hasOwnProperty('mensagem')) {
+			let data = dataOrdemServicoChoice.mensagem;
 
-	React.useEffect(()=>{
-    	const requesFiliais = async ()=>{
-    		await requestAllMotivoCancel();
-    	}
-    	
-    	requesFiliais();
+			if (data.hasOwnProperty('observacao')) {
+				obj.observacao = data.observacao;
+			}
+		}
 
-    }, []);
+		return obj;
+	}
+
+	React.useEffect(() => {
+		const requesFiliais = async () => {
+			await requestAllMotivoCancel();
+		}
+
+		requesFiliais();
+
+	}, []);
 
 
-	React.useEffect(()=>{
+	React.useEffect(() => {
 
-		if(dataOrdemServicoChoice && dataOrdemServicoChoice.hasOwnProperty('mensagem')){
+		if (dataOrdemServicoChoice && dataOrdemServicoChoice.hasOwnProperty('mensagem')) {
 			let data = dataOrdemServicoChoice.mensagem;
 			setDataitens(data?.item)
 		}
-		
+
 	}, [])
 
-    console.log('----------------------------- data pais ----------------------------------')
-    console.log(dataToFormOrdemServico())
-
-    if(error){
+	if (error) {
 		Swal.fire({
-		  	icon: "error",
-		  	title: "Oops...",
-		  	text: error,
-		  	footer: '',//'<a href="#">Why do I have this issue?</a>'
+			icon: "error",
+			title: "Oops...",
+			text: error,
+			footer: '',
 			confirmButtonColor: "#07B201",
 			//width:'20rem',
 		});
 	}
 
-	
-	return(
+	return (
 
 		<>
-			 <Formik 
+			<Formik
 
-                initialValues={{...dataToFormOrdemServico()}}
+				initialValues={{ ...dataToFormOrdemServico() }}
 				enableReinitialize={true}
-                validate={
-                    values=>{
+				validate={
+					values => {
 
-                        const errors = {}
+						const errors = {}
 
-						if(!values.observacao){
-						    errors.observacao="Obrigatório"
+						if (!values.observacao) {
+							errors.observacao = "Obrigatório"
 						}
-						
 
-                        return errors;
-                    }
-                }
+						return errors;
+					}
+				}
 
-                onSubmit={async(values, {setSubmitting})=>{
-                    /*setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                      }, 400);*/
-                      //alert('aqui')
-					 
-                     await sendData({...values});
-                }}
-            >
-                {
-                    (
-                        {
-                            values,
-                            errors,
-                            touched,
-                            handleChange,
-                            handleBlur,
-                            handleSubmit,
-                            isSubmitting
-                        }
-                    )=>(
+				onSubmit={async (values, { setSubmitting }) => {
 
-						<Modal  handleConcluir={()=>{handleSubmit(); }}  title={ 'Atualizar ordem de servico'} size="xs" dialogClassName={null} propsConcluir={{'disabled':loading}} labelConcluir={loading ? 'Salvando...' : 'Concluir'} aria-labelledby={'aria-labelledby'} labelCanelar="Fechar" show={showModalCriarOrdemServico} showHide={()=>{setShowModalCriarOrdemServico();setAtualizarCabecalhoOrdemServico(false);setIdOrdemServico(null);}}>
+					await sendData({ ...values });
+				}}
+			>
+				{
+					(
+						{
+							values,
+							errors,
+							touched,
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							isSubmitting
+						}
+					) => (
+
+						<Modal handleConcluir={() => { handleSubmit(); }} title={'Atualizar ordem de servico'} size="xs" dialogClassName={null} propsConcluir={{ 'disabled': loading }} labelConcluir={loading ? 'Salvando...' : 'Concluir'} aria-labelledby={'aria-labelledby'} labelCanelar="Fechar" show={showModalCriarOrdemServico} showHide={() => { setShowModalCriarOrdemServico(); setAtualizarCabecalhoOrdemServico(false); setIdOrdemServico(null); }}>
 							{
-									
-                                carregando && carregando==true
-                                ?
-                                    (<Load/>)
-                                :
-                                    (  
+
+								carregando && carregando == true
+									?
+									(<Load />)
+									:
+									(
 										<form onSubmit={handleSubmit}>
 											<Col xs="12" sm="12" md="12">
 												<span className="label_title_grup_forms">Dados básicos</span>
-												<hr/>
+												<hr />
 											</Col>
 
 											{
@@ -213,50 +187,46 @@ const AtualizarCabecalhoForm  = ({dataOrdemServicoChoice, setDataOrdemServico, s
 													<Field
 														data={
 															{
-																hasLabel:true,
-																contentLabel:'Observação *',
-																atributsFormLabel:{
+																hasLabel: true,
+																contentLabel: 'Observação *',
+																atributsFormLabel: {
 
 																},
-																atributsFormControl:{
-																	type:'text',
-																	name:'observacao',
-																	placeholder:'Observação',
-																	id:'observacao',
-																	onChange:handleChange,
-																	onBlur:handleBlur,
-																	value:values.observacao,
-																	className:estilos.input,
-																	size:"sm"
+																atributsFormControl: {
+																	type: 'text',
+																	name: 'observacao',
+																	placeholder: 'Observação',
+																	id: 'observacao',
+																	onChange: handleChange,
+																	onBlur: handleBlur,
+																	value: values.observacao,
+																	className: estilos.input,
+																	size: "sm"
 																},
-																options:[],
-																atributsContainer:{
-																	className:''
+																options: [],
+																atributsContainer: {
+																	className: ''
 																}
 															}
 														}
-													
+
 														component={FormControlInput}
 													></Field>
 													<ErrorMessage className="alerta_error_form_label" name="observacao" component="div" />
-													
+
 												</Col>
 											</Row>
-											
-											             
-
 										</form>
 
 									)
-														
-							}  
+							}
 
-                        </Modal>
-                    )
-                }
-            </Formik>
+						</Modal>
+					)
+				}
+			</Formik>
 		</>
 	)
 }
 
-export default AtualizarCabecalhoForm ;
+export default AtualizarCabecalhoForm;
